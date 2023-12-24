e0f485637a7816ff6a44eddf595b735bbe80f495
vicentepf78 <vicentepf78@gmail.com>
2023-12-24 08:36:22 -0300

    Criando ms-email - ajustando

--- src/main/java/com/ms/email/models/EmailModel.java
+++ src/main/java/com/ms/email/models/EmailModel.java
@@ -4,6 +4,7 @@ import com.ms.email.enums.StatusEmail;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Entity
 @Table(name = "TB_EMAIL")
 public class EmailModel implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
