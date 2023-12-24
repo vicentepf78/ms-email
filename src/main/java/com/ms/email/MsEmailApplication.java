b91abf98003f3cfc676d4cfe50f16296700cc132
vicentepf78 <vicentepf78@gmail.com>
2023-12-24 08:33:18 -0300

    Criando ms-email

---
+++ src/main/java/com/ms/email/MsEmailApplication.java
@@ -0,0 +1,12 @@
+package com.ms.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class MsEmailApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsEmailApplication.class, args);
+    }
+}
